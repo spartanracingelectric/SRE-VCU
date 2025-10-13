22495575a54a6d964fb689792b543c2cb94bd298
sghillie <shaungilmore2255@gmail.com>
2025-10-13 14:17:07 -0700

    updating .gitignore & fixing canManager

--- dev/main.c
+++ dev/main.c
@@ -408,7 +408,7 @@ void main(void)
         //canOutput_sendMCUControl(mcm0, FALSE);
 
         //Send debug data
-        canOutput_sendDebugMessage0(canMan, tps, bps, ic0, bms, sc);
+        canOutput_sendDebugMessage0(canMan, tps, bps, ic0, bms, sc, invFL, invFR);
         canOutput_sendDebugMessage1(canMan, invFL, invFR, invRL, invRR);
         //canOutput_sendSensorMessages();
         //canOutput_sendStatusMessages(mcm0);
