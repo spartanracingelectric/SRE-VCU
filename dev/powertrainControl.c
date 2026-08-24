6b851b8e8d5a8fba61581bbb8679d8e459849f58
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2026-08-23 18:19:16 -0700

    Fix code

--- dev/powertrainControl.c
+++ dev/powertrainControl.c
@@ -319,9 +319,10 @@ void Powertrain_calculateTorqueCommands(_Powertrain* me, TorqueEncoder *tps, Bra
         }
     }
 }
+}
 
 void Powertrain_TorqueVectoring(_Powertrain *me, TorqueEncoder *tps, BrakePressureSensor *bps, _DAQSensors *d1){
-    // TV goes here
+    return; 
 }
 
 void Powertrain_parseCanMessage(_Powertrain* me, IO_CAN_DATA_FRAME* pwCanMessage){
