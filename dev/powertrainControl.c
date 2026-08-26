78a55be2e6faaf4ac64117d7b13b38faaf6b3634
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2026-08-25 23:38:12 -0700

    60 Amps Starting

--- dev/powertrainControl.c
+++ dev/powertrainControl.c
@@ -243,8 +243,8 @@ void Powertrain_calculateTorqueCommands(_Powertrain* me, TorqueEncoder *tps, Bra
         else
         {
             me->useDutyCycle = FALSE;
-            me->motor[2]->current_mA = (sbyte4)((throttlePercent * MVP_MAX_CURRENT_mA)+40000);
-            me->motor[3]->current_mA = (sbyte4)((throttlePercent * MVP_MAX_CURRENT_mA))+40000;
+            me->motor[2]->current_mA = (sbyte4)((throttlePercent * MVP_MAX_CURRENT_mA) + 60000);
+            me->motor[3]->current_mA = (sbyte4)((throttlePercent * MVP_MAX_CURRENT_mA) + 60000);
             me->motor[2]->dutyCycle = 0;
             me->motor[3]->dutyCycle = 0;
         }
