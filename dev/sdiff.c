11cd989e2d7249d78667decd2c73498427b25c93
therealjohnwall1 <andyvvan@gmail.com>
2026-09-04 12:14:22 -0700

    define r0 for software differential still need to define constants

---
+++ dev/sdiff.c
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ * sdiff.c - Software Differential Revision(Pre-Torque Vectoring)
+ * Initial Author: Andy Van, Sellab Ahmadzai
+ ******************************************************************************
+ * Removes torque from inhub motors to mimic a differential
+ *
+ * R&D car for now, read update/revision notes at the end of file
+ ****************************************************************************/
+#include "sdiff.h"
+#include <math.h>
+#include <stdlib.h>
+
+static inline float4 clampf(float4 v, float4 lo, float4 hi) {
+  if (v < lo)
+    return lo;
+  if (v > hi)
+    return hi;
+  return v;
+}
+
+static inline float4 slew(float4 cur, float4 tgt, float4 ratePerSec) {
+  float4 step = ratePerSec * SDIFF_LOOP_PERIOD;
+  if (tgt > cur)
+    return (cur + step < tgt) ? cur + step : tgt;
+  if (tgt < cur)
+    return (cur - step > tgt) ? cur - step : tgt;
+  return cur;
+}
+
+SDiff *SDiff_new(void) {
+  SDiff *me = (SDiff *)malloc(sizeof(SDiff));
+  me->f_applied = 1.0f;
+  me->g_left_appl = 1.0f;
+  me->g_right_appl = 1.0f;
+  return me;
+}
+
+SDiff_Command s_diff_control(SDiff *me, float4 steering_deg,
+                             float4 t_driver) {
+  SDiff_Command cmd;
+
+  float4 delta = (steering_deg * DEG_TO_RAD) / STEERING_RATIO;
+  float4 delta_norm = clampf(fabsf(delta) / DELTA_MAX, 0.0f, 1.0f); // clamp norm value for steering ang
+
+  // friction budget
+  float4 f = clampf(1.0f - K_DERATE * delta_norm, F_MIN, 1.0f);
+
+  float4 g_in =  1.0f - K_INNER * delta_norm;
+  float4 g_out = 1.0f;
+  float4 g_left, g_right;
+
+  // left turn
+  if (delta > DEADBAND) {
+    g_left = g_in;
+    g_right = g_out;
+  }
+  // right turn
+  else if (delta < -DEADBAND) {
+    g_left = g_out;
+    g_right = g_in;
+  }
+  // straight
+  else {
+    g_left = 1.0f;
+    g_right = 1.0f;
+  }
+
+  // slew limit torque
+  me->f_applied = slew(me->f_applied, f, RATE);
+  me->g_left_appl = slew(me->g_left_appl, g_left, RATE);
+  me->g_right_appl = slew(me->g_right_appl, g_right, RATE);
+
+  cmd.left  = (sbyte2)clampf(t_driver * me->f_applied * me->g_left_appl, -2310.0f, 2310.0f);
+  cmd.right = (sbyte2)clampf(t_driver * me->f_applied * me->g_right_appl, -2310.0f, 2310.0f);
+
+  return cmd;
+}
--- dev/sdiff.h
+++ dev/sdiff.h
@@ -12,44 +12,51 @@
 
 #include "IO_Driver.h"
 
-// TODO: tune/define these, and set init values when driving and measuring
+#define DEG_TO_RAD 0.01745329252f
+
+// TODO: tune/define these, and set init values when driving and measuring PLS DONT FLASH THIS :(
+#define STEERING_RATIO 0
+#define DELTA_MAX 0 // max steering angle
 #define K_DERATE 0 // how aggro torque will decrease with steering angle
-#define F_MIN 0 // minimum torque floor, will cap the baseline torque reduction, 0 < for regen                   
-                // lowk try capping at 0 first then negative later irl
+#define K_INNER 0 // extra derate on inner (unloaded) wheel
+#define F_MIN 0  // minimum torque floor, will cap the baseline torque reduction, 0 < for
+    // regen lowk try capping at 0 first then negative later irl
 
-#define DEADBAND 0 // tolerance around center steering position
-#define MAX_DERATE 0 // max speed on how low the torques can drop for each motor
+#define DEADBAND 0   // tolerance around center steering position
+#define RATE 0 // max speed on how low the torques can drop for each motor
 #define SDIFF_LOOP_PERIOD 0.01f // 10ms off main
-
 
 static inline float4 clampf(float4 v, float4 lo, float4 hi);
 static inline float4 slew(float4 cur, float4 tgt, float4 ratePerSec);
 
-
 // running state of SDiff
 typedef struct _SDiff {
-    float4 f_applied;    // smoothed shared friction multiplier 
-    float4 g_left_appl;  // smoothed left 
-    float4 g_right_appl; // smoothed right 
+  float4 f_applied;    // smoothed shared friction multiplier
+  float4 g_left_appl;  // smoothed left
+  float4 g_right_appl; // smoothed right
 } SDiff;
 
+SDiff *SDiff_new(void);
 
-// AMK CAN protocol takes 16 bit signed field on bus
+SDiff_Command s_diff_control(SDiff *me, float4 steering_deg, float4 t_driver);
+
+// TODO: see what the custom inverters use idk any docs on that ngl
+// AMK CAN protocol takes 16 bit signed field on bus(not using)
 typedef struct _SDiff_Command {
-    sbyte2 left;   /* rear-left  torque request */
-    sbyte2 right;  /* rear-right torque request */
+  sbyte2 left;  // torque request 
+  sbyte2 right; // torque request
 } SDiff_Command;
-
-
 
 #endif
 
 /*
  * S_diff Revision 1:
  * Sensors: Steering angle Only
- * Primitve open loop system that will derate based off steering angle
- * logic: define what side the car is turning on, on that side reduce torque to the wheels on that side
+ * Primitive open loop system that will derate based off steering angle
+ * logic: define what side the car is turning on, on that side reduce torque to
+ * the wheels on that side
  *
  * S_diff Revision 2(TODO):
  * Sensors, Steering angle, 6axis IMU
+ *
  */
